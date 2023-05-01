23e4bd0d515cd6e9839a1b047a37e1e5cacd8332
Katherine West <katherinewest98@yahoo.com>
2023-05-01 12:38:21 -0500

    Complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 8;
+let num4 = 2;
+let mod = num3 / num4;
+
+let numbers = [1, 10, 20];
+let max = Math.max(...numbers);
